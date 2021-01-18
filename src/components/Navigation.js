b712d484638554d07fc0f9d255a986cf8dadc8e3
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-17 20:52:06 -0500

    restructured project section for mobile responsiveness

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,14 +5,24 @@ import Nav from "react-bootstrap/Nav";
 const Navigation = () => {
   return (
     <div>
-      <Navbar fixed="top" style={{ backgroundColor: "rgb(74, 5, 92)" }}>
-        <Navbar.Brand href="#landing">| TK |</Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-          <Nav.Link href="#resume">Resume</Nav.Link>
-        </Nav>
+      <Navbar
+        collapseOnSelect
+        fixed="top"
+        expand="md"
+        style={{ backgroundColor: "rgb(74, 5, 92)" }}
+      >
+        <Navbar.Brand href="#landing" style={{ marginLeft: "30px" }}>
+          | TK |
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto" style={{ marginRight: "30px" }}>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,19 +6,12 @@ import Button from "react-bootstrap/Button";
 
 const ProjectItem = ({ title, image, description, code, demo }) => {
   return (
-    <Col sm={6}>
+    <Col lg={4}>
+      <Image src={image} className="mac-img mx-auto d-block" />
       <Row>
-        <Col>
-          <Image src={image} className="mac-img" />
-        </Col>
-        <Col>
-          <h4
-            style={{ color: "black", fontSize: "16px", marginTop: "30px" }}
-            className="type"
-          >
-            {title}
-          </h4>
-          <h4 className="type">{description}</h4>
+        <Col className="text-center">
+          <h3 className="title">{title}</h3>
+          <p className="type">{description}</p>
           <Button className="project-btn" href={code} target="_blank">
             Code
           </Button>
@@ -32,3 +25,28 @@ const ProjectItem = ({ title, image, description, code, demo }) => {
 };
 
 export default ProjectItem;
+
+{
+  /* <Col sm={6}>
+      <Row>
+        <Col>
+          <Image src={image} className="mac-img" />
+        </Col>
+        <Col>
+          <p
+            
+            className="type"
+          >
+            {title}
+          </p>
+          <p className="type">{description}</p>
+          <Button className="project-btn" href={code} target="_blank">
+            Code
+          </Button>
+          <Button className="project-btn" href={demo} target="_blank">
+            Demo
+          </Button>
+        </Col>
+      </Row>
+    </Col>  */
+}
