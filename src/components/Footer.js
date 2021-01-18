3851801b7e4df5c7a2433f2804252e77d3717627
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-18 15:06:13 -0500

    media queries added for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div id="footer">
-      <p style={{ marginTop: "4px" }}>&copy; 2021 Tanner Kirkpatrick</p>
+      <p style={{ marginTop: "7px" }}>&copy; 2021 Tanner Kirkpatrick</p>
     </div>
   );
 };
